4fa04561724eea2c2613db1a3742f7b63b5dbecf
Andres Rodriguez <andresrdrgz96@gmail.com>
2025-01-27 15:34:48 -0500

    editar horas extras

--- pacifico/forms.py
+++ pacifico/forms.py
@@ -294,7 +294,7 @@ class FideicomisoForm(forms.ModelForm):
             'horasExtrasMonto': forms.NumberInput(attrs={
                 'placeholder': 'Horas Extras Monto',
                 'class': 'w-full bg-gray-100 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2',
-                'readonly': 'readonly',
+                
             }),
             'horasExtrasDcto': forms.CheckboxInput(attrs={
                 'class': 'w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded',
