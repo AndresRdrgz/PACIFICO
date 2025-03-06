9f6b05614779040b4e324b8321d70fc1abdea537
Andres Rodriguez <andresrdrgz96@gmail.com>
2025-03-06 12:09:29 -0500

    promocion marzo 300

--- financiera/settings.py
+++ financiera/settings.py
@@ -13,11 +13,12 @@ STATIC_URL = '/static/'
 DEBUG = True
 if DEBUG:
       DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        'default': dj_database_url.config(
+            default='postgresql://postgres:FP.h05t1l3@localhost:5432/pacifico',
+            conn_max_age=600
+        )
     }
+
 else:
     DATABASES = {
         'default': dj_database_url.config(
