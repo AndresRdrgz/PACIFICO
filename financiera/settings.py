10bbc77202abcc768edc5b051ea767fbce74739e
Andres Rodriguez <andresrdrgz96@gmail.com>
2025-06-24 12:17:20 -0500

    deploy with debug true

--- financiera/settings.py
+++ financiera/settings.py
@@ -10,15 +10,14 @@ STATIC_URL = '/static/'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-DEBUG = False
+DEBUG = True
 if DEBUG:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        'default': dj_database_url.config(
+            default='postgresql://postgres:FP.h05t1l3@localhost:5432/pacifico',
+            conn_max_age=600
+        )
     }
 
 else:
