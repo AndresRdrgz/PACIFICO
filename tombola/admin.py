597586709b4b8239edac822a9864b675b63a8cdc
jacr707 <jacr707@gmail.com>
2025-07-01 21:16:05 -0500

    se agrega carga maasiva a admin

--- tombola/admin.py
+++ tombola/admin.py
@@ -1,16 +1,28 @@
 from django.contrib import admin
-from unfold.admin import ModelAdmin
-from .models import Tombola, Boleto, FormularioTombola
+from .models import Tombola, Boleto, FormularioTombola, CargaMasiva
 
 # Register your models here.
 @admin.register(Tombola)
-class TombolaAdmin(ModelAdmin):
-    pass
+class TombolaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'fecha_evento')
+    list_filter = ('fecha_evento',)
+    search_fields = ('nombre',)
 
 @admin.register(Boleto)
-class BoletoAdmin(ModelAdmin):
-    pass
+class BoletoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tombola', 'cliente', 'canalOrigen', 'fecha_creacion')
+    list_filter = ('canalOrigen', 'tombola', 'fecha_creacion')
+    search_fields = ('cliente__nombreCliente', 'cliente__cedulaCliente')
 
 @admin.register(FormularioTombola)
-class FormularioTombolaAdmin(ModelAdmin):
-    pass
+class FormularioTombolaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'apellido', 'cedulaCliente', 'celular', 'fecha_creacion')
+    list_filter = ('sexo', 'sector', 'producto_interesado', 'oficial', 'tombola', 'fecha_creacion')
+    search_fields = ('nombre', 'apellido', 'cedulaCliente', 'celular', 'correo_electronico')
+
+@admin.register(CargaMasiva)
+class CargaMasivaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'archivo', 'cantidad_registros', 'usuario', 'fecha_subida')
+    list_filter = ('fecha_subida', 'usuario')
+    search_fields = ('usuario',)
+    ordering = ('-fecha_subida',)
